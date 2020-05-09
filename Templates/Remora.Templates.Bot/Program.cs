ce647a0379f7b7f0a81abe6c8757eda86b6ad5b0
Jarl Gullberg <jarl.gullberg@gmail.com>
2020-05-09 14:23:18 +0200

    Add missing documentation and headers.

--- Templates/Remora.Templates.Bot/Program.cs
+++ Templates/Remora.Templates.Bot/Program.cs
@@ -1,4 +1,26 @@
-﻿using System.Runtime.InteropServices;
+﻿//
+//  Program.cs
+//
+//  Author:
+//       Jarl Gullberg <jarl.gullberg@gmail.com>
+//
+//  Copyright (c) 2017 Jarl Gullberg
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Affero General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Affero General Public License for more details.
+//
+//  You should have received a copy of the GNU Affero General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using Discord;
 using Discord.WebSocket;
@@ -11,8 +33,15 @@ using Remora.Templates.Bot.Services;
 
 namespace Remora.Templates.Bot
 {
+    /// <summary>
+    /// The main class of the program.
+    /// </summary>
     internal class Program
     {
+        /// <summary>
+        /// The main entry point.
+        /// </summary>
+        /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
         public static async Task Main()
         {
             var hostBuilder = Host.CreateDefaultBuilder()
