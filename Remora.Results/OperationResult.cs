4e1708d75be38a19fef2d53f193e6ed8d1061d8a
Jarl Gullberg <jarl.gullberg@gmail.com>
2020-06-27 21:57:20 +0200

    Add a generic operation result.

---
+++ Remora.Results/OperationResult.cs
@@ -0,0 +1,62 @@
+﻿//
+//  OperationResult.cs
+//
+//  Author:
+//       Jarl Gullberg <jarl.gullberg@gmail.com>
+//
+//  Copyright (c) 2017 Jarl Gullberg
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Affero General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Affero General Public License for more details.
+//
+//  You should have received a copy of the GNU Affero General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+using System;
+using JetBrains.Annotations;
+
+namespace Remora.Results
+{
+    /// <summary>
+    /// Represents an attempt to perform a generic operation.
+    /// </summary>
+    [PublicAPI]
+    public sealed class OperationResult : ResultBase<OperationResult>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="OperationResult"/> class.
+        /// </summary>
+        private OperationResult()
+        {
+        }
+
+        /// <inheritdoc cref="ResultBase{TResultType}(string,Exception)"/>
+        [UsedImplicitly]
+        private OperationResult
+        (
+            string? errorReason,
+            Exception? exception = null
+        )
+            : base(errorReason, exception)
+        {
+        }
+
+        /// <summary>
+        /// Creates a new successful result.
+        /// </summary>
+        /// <returns>A successful result.</returns>
+        [PublicAPI, Pure, NotNull]
+        public static OperationResult FromSuccess()
+        {
+            return new OperationResult();
+        }
+    }
+}
